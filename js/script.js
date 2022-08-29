2ccbe77339e269438b3612081f980491983aa332
= <=>
2022-08-29 18:05:35 +0900

    pag2 스와이퍼 작업중

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 window.onload = function () {
 
 
-  
+
   let header = $('.header');
   let mainGnbDepth1_li_a = $('.maingnb-depth-1 > li > a');
   let logo = $('.logo');
@@ -23,7 +23,7 @@ window.onload = function () {
     mainGnbDepth1_li_a.css('color', '#000');
     logo.addClass('logo-open');
     allmenuBt.addClass('allmenu-bt-open');
-    
+
   });
 
   // 메인메뉴 닫기 기능
@@ -38,33 +38,20 @@ window.onload = function () {
   });
 
 
-  // allmenu depth2 오픈 기능
-  // let allmenuDepth1_li_a = $('.allmenu-depth-1 > li > a');
-  // let allmenuDepth2 = $('.allmenu-depth-2');
-
-
-
-  // allmenuDepth1_li_a.mouseenter(function(){
-  //   allmenuDepth2.css('visibility', 'visible');
-  // });
-
-  // allmenuDepth1_li_a.mouseleave(function(){
-  //   allmenuDepth2.css('visibility', 'hidden');
-  // });
 
 
   // allmenu 기능
-  
+
   let closeBt = $('.allmenu-close-bt');
   let allmenuWrap = $('.allmenu-wrap');
 
   // all menu 오픈기능
-  allmenuBt.click(function(){
+  allmenuBt.click(function () {
     allmenuWrap.addClass('allmenu-wrap-open');
   });
 
   // all menu 닫기기능
-  closeBt.click(function(){
+  closeBt.click(function () {
     allmenuWrap.removeClass('allmenu-wrap-open');
   });
 
@@ -90,6 +77,68 @@ window.onload = function () {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // pag2 slide
+
+  let sw_pag2 = new Swiper('.sw-pag2', {
+    // loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+  });
+
+
+
+  // 강제 swiper-slide 크기 조정
+  let pag2A = $('.sw-pag2-slideA');
+  let pag2A_ba = $('.sw-pag2').find('.swiper-slide');
+  let pag2A_active = $('.sw-pag2').find('.swiper-slide-active');
+
+  console.log(pag2A_ba);
+  console.log(pag2A_active);
+
+  function myfnfn1(el) {
+    el.css('width', '180px');
+    console.log('1');
+  }
+
+  function myfnfn2(el) {
+    el.css('width', '500px');
+    console.log('2');
+  }
+  myfnfn1(pag2A_ba);
+  myfnfn2(pag2A_active);
+
+
+  // 데이터 
+  let slideData = [
+    {
+      goodname: "스페니쉬",
+      img: url('')
+    }, 
+    {
+      goodname: "롱블랙",
+      img: url('')
+    }, 
+    {
+      goodname: "허니레몬티",
+      img: url('')
+    },
+    {
+      goodname: "스피치",
+      img: url('')
+    }
+  ];
+
+
+
+
+
+
+
+
+
+
+
 
 
   // // gotobox
@@ -117,4 +166,3 @@ window.onload = function () {
 
 
 };
-
