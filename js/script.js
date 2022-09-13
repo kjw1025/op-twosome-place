76a2339ebbca50e170319c51630e5d2a38c01c00
= <=>
2022-09-13 18:10:00 +0900

    pag2 오류 수정중

--- js/script.js
+++ js/script.js
@@ -7,45 +7,184 @@ $(document).ready(function () {
 
 window.onload = function () {
 
-  fetchData = () => {
+
+  let pag2TxtBoxA = $('.pag-2-txtbox a');
+  let datArr = [];
+
+  function fetchData() {
     axios
       .get("data/pag2_data.json")
       .then(function (response) {
-        console.log("data : ", response.data);
+        // console.log("data : ", response.data);
 
-        let coffeeData = response.data.coffee;
-        let dessertData = response.data.dessert;
-        let deliData = response.data.deli;
+        datArr.push(response.data.coffee);
+        datArr.push(response.data.dessert);
+        datArr.push(response.data.deli);
 
-        console.log(coffeeData);
-        console.log(dessertData);
-        console.log(deliData);
-
-        let tempHtml = '';
-        for (i = 0; i < coffeeData.length; i++) {
-
-            temp = `
-              <div class="sw-hs-slide">
-                <a href="#">
-                  <img src="images/${coffeeData[i].link}" alt="${coffeeData[i].name}">
-                </a>
-              </div>
-            `;
-
-          tempHtml += temp
-
-        }
-        document.getElementById("sw-hs-wrap").innerHTML = tempHtml
-
+        makeSlide(0)
       })
       .catch(err => console.log("에러 발생 :", err));
+  }
+
+
+  function makeSlide(_index) {
+    let data = datArr[_index];
+    let tempHtml = '';
+    for (i = 0; i < data.length; i++) {
+
+      temp = `
+          <div class="sw-hs-slide">
+            <a href="#">
+              <img src="images/${data[i].link}" alt="${data[i].name}">
+            </a>
+          </div>
+        `;
+
+      tempHtml += temp
+
+    }
+    document.getElementById("sw-hs-wrap").innerHTML = tempHtml;
+
+    playSlide();
 
   }
+
+  function playSlide() {
+
+    // 슬라이드 구현
+    let swhsSlide = $('.sw-hs-slide');
+    let swhsWMax = 488;
+    let swhsW = 176;
+    let swhsD = 30;
+    let posX = [];
+    let tempPos = 0;
+    // 최소 위치
+    let swhsMin = -(swhsW + swhsD);
+    let swhsTotal = swhsSlide.length;
+    let swhsIndex = 0;
+    let swhsDir = 'left';
+    // 초기 위치 배열
+    $.each(swhsSlide, function (index, item) {
+      if (index == 0) {
+        posX[index] = 0;
+        tempPos = swhsWMax + swhsD
+      } else {
+        posX[index] = tempPos;
+        tempPos = tempPos + swhsD + swhsW;
+      }
+    });
+    posX[swhsTotal] = tempPos;
+
+    swhsSlide.eq(swhsIndex).addClass('sw-hs-slide-active')
+
+    // 초기 셋팅
+    $.each(swhsSlide, function (index, item) {
+      $(this).css('left', posX[index]);
+      $(this).attr('data-index', index)
+    })
+
+    let swhsNext = $('.sw-pag2-next');
+    let swhsPrev = $('.sw-pag2-prev');
+
+    swhsNext.click(function () {
+      swhsIndex++;
+      if (swhsIndex >= swhsTotal) {
+        swhsIndex = 0;
+      }
+      swhsDir = 'left';
+      swhsMove();
+    })
+
+    swhsPrev.click(function () {
+      swhsIndex--;
+      if (swhsIndex < 0) {
+        swhsIndex = swhsTotal - 1;
+      }
+      swhsDir = 'right';
+      swhsMove();
+    })
+
+    function swhsMove() {
+
+      if (swhsDir == 'left') {
+        $.each(swhsSlide, function (index, item) {
+          let tempIndex = $(this).attr('data-index');
+          let tempX = tempIndex - 1;
+          let tgX = 0;
+          if (tempX < 0) {
+            $(this).css('left', posX[swhsTotal]);
+            $(this).removeClass('sw-hs-slide-active');
+            $(this).attr('data-index', swhsTotal - 1);
+            tgX = posX[swhsTotal - 1];
+          } else if (tempX == 0) {
+            $(this).attr('data-index', tempX);
+            tgX = posX[tempX];
+          } else {
+            $(this).attr('data-index', tempX);
+            tgX = posX[tempX];
+          }
+
+          gsap.to($(this), 0.1, {
+            left: tgX,
+            onComleted: function () {
+              swhsSlide.eq(swhsIndex).addClass('sw-hs-slide-active')
+            }
+          });
+
+        })
+
+      } else {
+
+        $.each(swhsSlide, function (index, item) {
+          let tempIndex = $(this).attr('data-index');
+          tempIndex = parseInt(tempIndex);
+
+          let tempX = tempIndex + 1;
+          let tgX = 0;
+          if (tempX >= swhsTotal) {
+            $(this).css('left', swhsMin);
+            $(this).attr('data-index', 0);
+            tempX = 0;
+            tgX = posX[0];
+          } else if (tempX == 1) {
+            $(this).removeClass('sw-hs-slide-active');
+            $(this).attr('data-index', tempX);
+            tgX = posX[tempX];
+          } else {
+            $(this).attr('data-index', tempX);
+            tgX = posX[tempX];
+          }
+          gsap.to($(this), 0.1, {
+            left: tgX,
+            onComleted: function () {
+              swhsSlide.eq(swhsIndex).addClass('sw-hs-slide-active')
+            }
+          });
+        })
+      }
+    }
+  }
+
 
   fetchData();
 
 
-      
+  $.each(pag2TxtBoxA, function (index, item) {
+    $(this).click(function (event) {
+      event.preventDefault();
+      makeSlide(index);
+
+      $(this).find('h4').removeClass('pag2-mainh4-focus');
+      $(this).find('p').removeClass('pag2-mainsubtxt-focus');
+
+      $(this).find('h4').addClass('pag2-mainh4-focus');
+      $(this).find('p').addClass('pag2-mainsubtxt-focus');
+
+    })
+  });
+
+
+
 
 
   let header = $('.header');
@@ -120,117 +259,6 @@ window.onload = function () {
 
 
   // pag2 slide
-
-  // 슬라이드 구현
-  let swhsSlide = $('.sw-hs-slide');
-  let swhsWMax = 488;
-  let swhsW = 176;
-  let swhsD = 30;
-  let posX = [];
-  let tempPos = 0;
-  // 최소 위치
-  let swhsMin = -(swhsW + swhsD);
-  let swhsTotal = swhsSlide.length;
-  let swhsIndex = 0;
-  let swhsDir = 'left';
-  // 초기 위치 배열
-  $.each(swhsSlide, function (index, item) {
-    if (index == 0) {
-      posX[index] = 0;
-      tempPos = swhsWMax + swhsD
-    } else {
-      posX[index] = tempPos;
-      tempPos = tempPos + swhsD + swhsW;
-    }
-  });
-  posX[swhsTotal] = tempPos;
-
-  // 초기 셋팅
-  $.each(swhsSlide, function (index, item) {
-    $(this).css('left', posX[index]);
-    $(this).attr('data-index', index)
-  })
-
-  let swhsNext = $('.sw-pag2-next');
-  let swhsPrev = $('.sw-pag2-prev');
-
-  swhsNext.click(function () {
-    swhsIndex++;
-    if (swhsIndex >= swhsTotal) {
-      swhsIndex = 0;
-    }
-    swhsDir = 'left';
-    swhsMove();
-  })
-
-  swhsPrev.click(function () {
-    swhsIndex--;
-    if (swhsIndex < 0) {
-      swhsIndex = swhsTotal - 1;
-    }
-    swhsDir = 'right';
-    swhsMove();
-  })
-
-  function swhsMove() {
-
-    if (swhsDir == 'left') {
-      $.each(swhsSlide, function (index, item) {
-        let tempIndex = $(this).attr('data-index');
-        let tempX = tempIndex - 1;
-        let tgX = 0;
-        if (tempX < 0) {
-          $(this).css('left', posX[swhsTotal]);
-          $(this).removeClass('sw-hs-slide-active');
-          $(this).attr('data-index', swhsTotal - 1);
-          tgX = posX[swhsTotal - 1];
-        } else if (tempX == 0) {
-          $(this).attr('data-index', tempX);
-          tgX = posX[tempX];
-        } else {
-          $(this).attr('data-index', tempX);
-          tgX = posX[tempX];
-        }
-
-        gsap.to($(this), 0.1, {
-          left: tgX,
-          onComleted: function () {
-            swhsSlide.eq(swhsIndex).addClass('sw-hs-slide-active')
-          }
-        });
-
-      })
-
-    } else {
-
-      $.each(swhsSlide, function (index, item) {
-        let tempIndex = $(this).attr('data-index');
-        tempIndex = parseInt(tempIndex);
-
-        let tempX = tempIndex + 1;
-        let tgX = 0;
-        if (tempX >= swhsTotal) {
-          $(this).css('left', swhsMin);
-          $(this).attr('data-index', 0);
-          tempX = 0;
-          tgX = posX[0];
-        } else if (tempX == 1) {
-          $(this).removeClass('sw-hs-slide-active');
-          $(this).attr('data-index', tempX);
-          tgX = posX[tempX];
-        } else {
-          $(this).attr('data-index', tempX);
-          tgX = posX[tempX];
-        }
-        gsap.to($(this), 0.1, {
-          left: tgX,
-          onComleted: function () {
-            swhsSlide.eq(swhsIndex).addClass('sw-hs-slide-active')
-          }
-        });
-      })
-    }
-  }
 
 
 
