f05e33176c6564ddcee2d0bd5202cc2503b1efb2
= <=>
2022-10-13 14:48:25 +0900

    모달창 추가

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  // modal 기능
+  let modalWrap = $(".modal-wrap");
+  let modalClose = "modal-close-active";
+  let modalCloseBt = $(".modal-close");
+
+  modalCloseBt.click(() => {
+    modalWrap.addClass(modalClose);
+  });
+
   // 마우스 휠 코드
   let section = $(".wrap > section");
   let footer = $(".footer");
